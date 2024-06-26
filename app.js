96b0505c7f155b8e170df3369fcd8200724a577d
Charuka147 <dilipcharuka147@gmail.com>
2024-06-26 16:20:17 +0530

    LAST COMMIT

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-/*class Customer{
-    name;
-    age;
-    address;
-
-    constructor(name,age,address){
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-
-    setName(name){
-        this.name = name;
-    }
-    getName(){
-        return this.name ;
-    }
-}
-    let customer01 = new Customer("Saman",12,"Panadura");
-
-    console.log(customer01.getName());
-*/
 
 document.getElementById("btnguestCheck").onclick = function(){
 
-   
-    //let randomNumber = Math.random();
-    //randomNumber = randomNumber *11;
-    //randomNumber = Math.floor(randomNumber);
     if(document.getElementById("Guess").value == randomNumber){
         alert ("Well Done !!!.Your Answer is Correct");
     }
